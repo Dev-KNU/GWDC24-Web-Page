e400d1ccce7c0a2b5d0b78c9377bc0f89d6687af
gunyu1019 <gunyu1019@gmail.com>
2024-01-24 14:56:23 +0900

    [Feat] Add Header Navbar

--- src/components/NavbarHeader.vue
+++ src/components/NavbarHeader.vue
@@ -1,23 +1,32 @@
 <script setup lang='ts'>
-
+import logo from '@/assets/image/logo1.svg';
 </script>
 
 <template>
     <BNavbar toggleable="lg" container="true">
         <BNavbarBrand href="/">
-            <span>GWDC</span>
-            <span>24</span>
+            <img :src='logo' alt='GWDC logo version1' />
         </BNavbarBrand>
     </BNavbar>
 </template>
 
 <style lang='scss' scoped>
+@import "@/assets/color";
+$color-backgroun-navbar: rgba($color-white, .1);
+
 .navbar {
-    z-index: 1;
+    display: flex;
+    width: 1194px;
+    padding: 20px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 10px;
+
     position: absolute;
     top: 0;
-    width: 100%;
+    z-index: 1;
 
-    background: ;
+    background: $color-backgroun-navbar;
 }
 </style>
