4d1f3fc5ac994dfaf022d2ab6cd6146c5f5a7497
Mule129 <mule01129@gmail.com>
2024-01-24 20:56:07 +0900

    [Feat] Feat SpeakerMember.vue

--- src/components/Speaker.vue
+++ src/components/Speaker.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import SpeakerMember from '@/components/SpeakerMember.vue'
+
 import speaker_1 from '@/assets/image/speaker_1.svg'
 import speaker_2 from '@/assets/image/speaker_2.svg'
 import speaker_3 from '@/assets/image/speaker_3.svg'
 import speaker_4 from '@/assets/image/speaker_4.svg'
 import speaker_blur_img from '@/assets/image/speaker_blur_img.svg'
+
+
 </script>
 
 <template>
@@ -15,54 +19,10 @@ import speaker_blur_img from '@/assets/image/speaker_blur_img.svg'
             </div>
         </div>
         <div class='speakers-container'>
-            <div class='speaker-container'>
-                <div>
-                    <img :src='speaker_1' alt='GWDC24 speaker_1'>
-                </div>
-                <div class='speaker-text'>
-                    <div class='speaker-text-info'>
-                        <span>연사1</span>
-                        <span class='speaker-text-develop'>아직 공개되지 않았습니다</span>
-                    </div>
-                    <span>궁금해서 여기까지 오셨다면 축하드립니다! 이스터에그를 발견하셨습니다.</span>
-                </div>
-            </div>
-            <div class='speaker-container'>
-                <div>
-                    <img :src='speaker_2' alt='GWDC24 speaker_2'>
-                </div>
-                <div class='speaker-text'>
-                    <div class='speaker-text-info'>
-                        <span>연사2</span>
-                        <span class='speaker-text-develop'>곧 공개될 예정이니 기다려주세요</span>
-                    </div>
-                    <span>이곳 말고도 다른 곳에 이스터에그가 있으니, 잘 탐색해주세요 :)</span>
-                </div>
-            </div>
-            <div class='speaker-container'>
-                <div>
-                    <img :src='speaker_3' alt='GWDC24 speaker_3'>
-                </div>
-                <div class='speaker-text'>
-                    <div class='speaker-text_info'>
-                        <span>연사3</span>
-                        <span class='speaker-text-develop'>3월 초, 기대해주세요</span>
-                    </div>
-                    <span>이 웹페이지는 Vue + TS 기반으로 만들어졌습니다.</span>
-                </div>
-            </div>
-            <div class='speaker-container'>
-                <div>
-                    <img :src='speaker_4' alt='GWDC24 speaker_4'>
-                </div>
-                <div class='speaker-text'>
-                    <div class='speaker-text-info'>
-                        <span>연사4</span>
-                        <span class='speaker-text-develop'>현재 연사 섭외중입니다</span>
-                    </div>
-                    <span>이 이스터에그는 오거나이저 이용현님의 아이디어로 제작되었습니다</span>
-                </div>
-            </div>
+            <SpeakerMember :SpeakerName='"연사1"' :SpeakerInfo='"아직 공개되지 않았습니다"' :SpeakerSession='"궁금해서 여기까지 오셨다면 축하드립니다! 이스터에그를 발견하셨습니다"' :SpeakerImg='speaker_1'/>
+            <SpeakerMember :SpeakerName='"연사2"' :SpeakerInfo='"곧 공개될 예정이니 기다려주세요"' :SpeakerSession='"이곳 말고도 다른 곳에 이스터에그가 있으니, 잘 탐색해주세요 :)"' :SpeakerImg='speaker_2'/>
+            <SpeakerMember :SpeakerName='"연사3"' :SpeakerInfo='"3월 초, 기대해주세요"' :SpeakerSession='"이 웹페이지는 Vue + TS 기반으로 만들어졌습니다"' :SpeakerImg='speaker_3'/>
+            <SpeakerMember :SpeakerName='"연사4"' :SpeakerInfo='"현재 연사 섭외중입니다"' :SpeakerSession='"이 이스터에그는 오거나이저 이용현님의 아이디어로 제작되었습니다"' :SpeakerImg='speaker_4'/>
         </div>
     </div>
 </template>
@@ -81,6 +41,7 @@ $color-backgroun-speaker: $color-primary-100;
 }
 
 .speakers-blur {
+    width: 100%;
     height: 100%;
     display: flex;
     align-items: center;
@@ -130,34 +91,6 @@ $color-backgroun-speaker: $color-primary-100;
 
     font-size: 14px;
     font-weight: 500;
-
-    img {
-        width: 80px;
-    }
-
-    .speaker-container {
-        display: flex;
-        flex-flow: row;
-        gap: 12px;
-        align-items: center;
-
-        .speaker-text {
-            display: flex;
-            flex-flow: column;
-            gap: 8px;
-
-            .speaker-text-info {
-                display: flex;
-                flex-flow: column;
-                gap: 4px;
-
-                .speaker-text-develop {
-                    font-size: 12px;
-                    font-weight: 400;
-                }
-            }
-        }
-    }
 }
 
 </style>
