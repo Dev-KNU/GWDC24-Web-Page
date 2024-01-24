c5e9abe1b8f7142fcdb027d01c2373cc1570f342
Mule129 <mule01129@gmail.com>
2024-01-24 19:05:30 +0900

    [BugFix] Fixed Speaker scss setting and speaker info text

--- src/components/Speaker.vue
+++ src/components/Speaker.vue
@@ -20,11 +20,11 @@ import speaker_blur_img from '@/assets/image/speaker_blur_img.svg'
                     <img :src='speaker_1' alt='GWDC24 speaker_1'>
                 </div>
                 <div class='speaker-text'>
-                    <div class='speaker-text_info'>
-                        <span>박지영</span>
-                        <span>Python 개발 | Dev.KNU 관리자</span>
+                    <div class='speaker-text-info'>
+                        <span>연사1</span>
+                        <span class='speaker-text-develop'>아직 공개되지 않았습니다</span>
                     </div>
-                    <span>Python과 AI: tensorflow 기초 가이드</span>
+                    <span>궁금해서 여기까지 오셨다면 축하드립니다! 이스터에그를 발견하셨습니다.</span>
                 </div>
             </div>
             <div class='speaker-container'>
@@ -32,11 +32,11 @@ import speaker_blur_img from '@/assets/image/speaker_blur_img.svg'
                     <img :src='speaker_2' alt='GWDC24 speaker_2'>
                 </div>
                 <div class='speaker-text'>
-                    <div class='speaker-text_info'>
-                        <span>박지영</span>
-                        <span>Python 개발 | Dev.KNU 관리자</span>
+                    <div class='speaker-text-info'>
+                        <span>연사2</span>
+                        <span class='speaker-text-develop'>곧 공개될 예정이니 기다려주세요</span>
                     </div>
-                    <span>Python과 AI: tensorflow 기초 가이드</span>
+                    <span>이곳 말고도 다른 곳에 이스터에그가 있으니, 잘 탐색해주세요 :)</span>
                 </div>
             </div>
             <div class='speaker-container'>
@@ -45,10 +45,10 @@ import speaker_blur_img from '@/assets/image/speaker_blur_img.svg'
                 </div>
                 <div class='speaker-text'>
                     <div class='speaker-text_info'>
-                        <span>박지영</span>
-                        <span>Python 개발 | Dev.KNU 관리자</span>
+                        <span>연사3</span>
+                        <span class='speaker-text-develop'>3월 초, 기대해주세요</span>
                     </div>
-                    <span>Python과 AI: tensorflow 기초 가이드</span>
+                    <span>이 웹페이지는 Vue + TS 기반으로 만들어졌습니다.</span>
                 </div>
             </div>
             <div class='speaker-container'>
@@ -56,11 +56,11 @@ import speaker_blur_img from '@/assets/image/speaker_blur_img.svg'
                     <img :src='speaker_4' alt='GWDC24 speaker_4'>
                 </div>
                 <div class='speaker-text'>
-                    <div class='speaker-text_info'>
-                        <span>박지영</span>
-                        <span>Python 개발 | Dev.KNU 관리자</span>
+                    <div class='speaker-text-info'>
+                        <span>연사4</span>
+                        <span class='speaker-text-develop'>현재 연사 섭외중입니다</span>
                     </div>
-                    <span>Python과 AI: tensorflow 기초 가이드</span>
+                    <span>3월 9일 컨퍼런스, 기대해주세요!</span>
                 </div>
             </div>
         </div>
@@ -69,10 +69,15 @@ import speaker_blur_img from '@/assets/image/speaker_blur_img.svg'
 
 <style lang="scss" scoped>
 @import '@/assets/color';
+@import "pretendard/dist/web/static/pretendard.css";
+
 $color-backgroun-speaker: $color-primary-100;
 
 .speakers {
+    max-width: 340px;
+
     position: relative;
+    font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
 }
 
 .speakers-blur {
@@ -83,15 +88,22 @@ $color-backgroun-speaker: $color-primary-100;
     position: absolute;
     z-index: 1;
 
-    backdrop-filter: blur(10px);
+    backdrop-filter: blur(5px);
 
     .speakers-blur-text {
         display: flex;
         flex-flow: column;
         align-items: center;
+        padding: 10px;
         gap: 24px;
 
         position: relative;
+
+        font-size: 24px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        text-align: center;
 
         img {
             display: flex;
@@ -114,22 +126,35 @@ $color-backgroun-speaker: $color-primary-100;
     flex-direction: column;
     align-items: flex-start;
     gap: 32px;
+    margin: 10px;
+
+    font-size: 14px;
+    font-weight: 500;
+
+    img {
+        width: 80px;
+    }
 
     .speaker-container {
         display: flex;
         flex-flow: row;
-        gap: 17px;
+        gap: 12px;
         align-items: center;
 
         .speaker-text {
             display: flex;
             flex-flow: column;
-            gap: 12px;
+            gap: 8px;
 
-            .speaker-text_info {
+            .speaker-text-info {
                 display: flex;
                 flex-flow: column;
                 gap: 4px;
+
+                .speaker-text-develop {
+                    font-size: 12px;
+                    font-weight: 400;
+                }
             }
         }
     }
