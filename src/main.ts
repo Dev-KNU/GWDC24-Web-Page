c9b0d0548c23b9b355f4ae0034e62250d39e242e
gunyu1019 <gunyu1019@users.noreply.github.com>
2024-02-03 06:12:58 +0000

    Apply formatting changes

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import './assets/base.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueGtag from 'vue-gtag-next';
+import VueGtag from 'vue-gtag-next'
 import BootstrapVueNext from 'bootstrap-vue-next'
 
 const app = createApp(App)
 
 app.use(VueGtag, {
-    property: { id: "G-TQ4VDNCPH3" }
-});
+    property: { id: 'G-TQ4VDNCPH3' }
+})
 app.use(BootstrapVueNext)
 app.mount('#app')
